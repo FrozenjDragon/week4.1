e0d17365036f639b44a71dba6817f76cfac99780
FrozenjDragon <j.islomov@newuu.uz>
2025-09-15 13:45:01 +0500

    week 4.1;

---
+++ main.cpp
@@ -0,0 +1,200 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int number;
+//     cin >> number;
+//     if (number > 0)
+//         cout <<"Positive number";
+//     else cout << "Negative number";
+//
+//
+//     return 0;
+// }
+
+
+    //     int number;
+    //     cin >> number;
+    //     cout << "Enter an integer: ";
+    //
+    //     if (number % 2 == 0)
+    //         cout << number << " is even." << endl;
+    //     else
+    //         cout << number << " is odd." << endl;
+    //
+    //     return 0;
+// int x,y;
+//     cin>>x>>y;
+//     if(x>y) {
+//         cout<<"max="<<x<<endl;
+//     }
+//         else {
+//             cout<<"max="<<y<<endl;
+//         }
+//
+//
+// return 0;
+// }
+
+
+// int main() {
+//     int x,y;
+//     cin>>x>>y;
+//     if (x==y) {
+//         cout<<"Numbers are equal"<<endl;
+//     }
+//         else if (x>y) {
+//             cout<<"max"<<x;
+//         }
+//         else if (x<y) {
+//             cout<<"max"<<y;
+//         }
+//             return 0;
+//         }
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+// //     int a, b, c;
+// //     cin >> a >> b >> c;
+// //
+// //     int max;
+// //
+// //     if (a > b) {
+// //         if (a > c)
+// //             max = a;
+// //         else
+// //             max = c;
+// //     } else {
+// //         if (b > c)
+// //             max = b;
+// //         else
+// //             max = c;
+// //     }
+// //
+// //     cout << "max: " << max << endl;
+// //
+// //     return 0;
+// // }
+// #include <iostream>
+// using namespace std;
+//
+//  int main() {
+// int year;
+//      cin >> year;
+//      if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//          cout << year <<" is a leap year";
+//      }
+//      else {
+//          cout << year <<"is not a leap year";
+//      }
+//
+//
+//
+//  }
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+// int mark;
+//     cin >> mark;
+//     if (mark>=50) {
+//         cout<<"Pass"<<endl;
+//     }
+//     else {
+//         cout<<"Fail"<<endl;
+//     }
+// }
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     int number;
+//         cin >> number;
+//         if (number > 0) {
+//             cout <<"Positive number";
+//         }
+//       else if (number < 0) {
+//           cout << "Negative number";
+//       }
+//     else if (number == 0) {
+//         cout << "Zero number";
+//     }
+//
+//
+//
+// }
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     int mark;
+//     cin >> mark;
+//     if (mark < 60) {
+//         cout << "F";
+//     }
+//     else if (mark >= 60 && mark <= 69) {
+//         cout << "D";
+//     }
+//     else if (mark >= 70 && mark <= 79) {
+//         cout << "C";
+//     }
+//     else if (mark >= 80 && mark <= 89) {
+//         cout << "B";
+//     }
+//     else if (mark >= 90 && mark <= 100) {
+//         cout << "A";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     int n;
+//     cin >> n;
+//     int abs_v = (n >= 0) ? n : -n;
+//     cout  << abs_v;
+//
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     int n;
+//     cin >> n;
+//     if (n % 3 == 0   && n % 5 == 0 ) {
+//         cout <<"Devisible by both";
+//     }
+//         else {
+//             cout <<"Not Devisible by both";
+//         }
+//
+// }
+// 20
+// #include <iostream>
+//     using namespace std;
+//
+//     int main() {
+//         int a, b;
+//         cin >> a >> b;
+//             int remainder =a % b;
+//             cout << remainder ;
+//
+//         return 0;
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
